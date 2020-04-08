3b4c00adb1448552c840fc9209aad4ebbbcc7c66
BuildTools <unconfigured@null.spigotmc.org>
2020-04-08 22:06:54 +0200

    first commit

---
+++ app/cart/cart.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+items= [];
+checkoutForm;
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+   }
+
+  ngOnInit() {
+    // this.items = this.cartService.getItems();
+    this.items = JSON.parse(localStorage.getItem('items'));
+    
+  }
+
+  onSubmit(customerData){
+    // Process purchase
+    this.items = this.cartService.clearCart();
+    localStorage.clear();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+  }
+
+}
